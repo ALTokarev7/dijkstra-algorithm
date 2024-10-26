1d5425c6cf49f87c75671440fdedac5cc5a32e9f
Александр Токарев <sasha_tokarev_02@mail.ru>
2024-10-26 16:03:10 +0300

    add project

---
+++ dijkstra/dijkstra.py
@@ -0,0 +1,59 @@
+from queue import PriorityQueue
+from dataclasses import dataclass, field
+import queue
+from re import search
+from sys import path
+
+@dataclass(order=True)
+class Vertex:
+    cost: float
+    name: str=field(compare=False)
+
+@dataclass(order=True)
+class Path:
+    cost: float
+    prev: str=field(compare=False)
+
+class Dijkstra(object):
+    def __init__(self):
+        self.queue = PriorityQueue()
+
+    def set_graph(self, graph: dict[str, dict[str, int]]) -> None:
+        self.graph = graph
+
+    def create_path_table(self, start: str) -> None:
+        self.path_table: dict[str, Path] = dict()
+        for vertex in self.graph:
+            self.path_table[vertex] = Path(float('inf'), '-')
+        self.path_table[start] = Path(0, 'strat')
+
+    def sum_costs(self, first: float, second: float) -> float:
+        if self.opt_crit == 'max':
+            return max(first, second)
+        else:
+            return first+second
+
+    def run(self, start: str, end: str, opt_crit: str='max') -> tuple[dict[str, Path], list[str]]:
+        self.opt_crit = opt_crit
+        self.create_path_table(start)
+
+        searched = set()
+        active = Vertex(cost=0, name=start)
+        while(active.name != end):
+            for vertex_name, vertex_cost in self.graph[active.name].items():
+                new_cost = self.sum_costs(active.cost, vertex_cost)
+                if new_cost < self.path_table[vertex_name].cost:
+                    self.path_table[vertex_name] = Path(new_cost, active.name)
+                    self.queue.put(Vertex(new_cost, vertex_name))
+
+            active = self.queue.get()
+            while active.name in searched:
+                active = self.queue.get()
+            searched.add(active.name)
+
+        path = [end]
+        cur = end
+        while cur != start:
+            path.append(self.path_table[cur].prev)
+            cur = self.path_table[cur].prev
+        return self.path_table, path[::-1]
---
+++ dijkstra/main.py
@@ -0,0 +1,34 @@
+from dijkstra import Dijkstra
+
+
+def enter_graph():
+    stop = False
+    graph = {}
+    while not stop:
+        v = input('Enter a vertex name: ')
+        graph[v] = {}
+        add = True if input('Add neighbors? [y/n] ') == 'y' else False
+        while add:
+            v_next_name = input('vertex name: ')
+            v_next_cost = input('path cost: ')
+            graph[v][v_next_name] = float(v_next_cost)
+            graph[v_next_name] = {}
+            add = True if input('Add more neighbors?  [y/n]: ') == 'y' else False
+        stop = False if input('Add more vertices?  [y/n]: ') == 'y' else True
+
+    print(graph)
+    return graph
+
+def main() -> None:
+    graph = enter_graph()
+    finder = Dijkstra()
+    finder.set_graph(graph)
+    start = input('Enter start name: ')
+    end = input('Enter end name: ')
+    crit = input('Сriterion type [add/max]: ')
+    table, path = finder.run(start, end, crit)
+    print('Path quality criterion: ', table[end].cost)
+    print('Path:', '->'.join(path))
+
+if __name__ == '__main__':
+    main()
